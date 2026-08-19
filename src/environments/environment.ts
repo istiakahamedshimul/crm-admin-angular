b563be19a2ad7e6bcd531c1b35110101b4779ebd
Istiak Ahmed <22312104@iubat.edu>
2026-08-19 21:48:00 +0600

    Point admin app to new CRM server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:5000/api',
-  swaggerUrl: 'http://localhost:5000/swagger',
+  // apiBaseUrl: 'http://localhost:5000/api',
+  // swaggerUrl: 'http://localhost:5000/swagger',
+  apiBaseUrl: 'http://20.219.131.147:5000/api',
+  swaggerUrl: 'http://20.219.131.147:5000/swagger',
   geminiApiKey: ''
 };
